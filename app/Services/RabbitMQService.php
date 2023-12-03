2d31218a20428ada8a2857528e5267bf03fff228
Lucas Mendes <lucas.mendes@4linux.com.br>
2023-12-03 13:14:51 -0300

    refactor: adjusting the rabbitmq service and adding configuration file for rabbit

--- app/Services/RabbitMQService.php
+++ app/Services/RabbitMQService.php
@@ -11,34 +11,36 @@ use Closure;
 class RabbitMQService
 {
     private AMQPStreamConnection $connection;
-    private string $host;
-    private string $port;
-    private string $user;
-    private string $password;
+
+    private AMQPChannel $channel;
 
     public function __construct()
     {
-        $this->host = getenv('RABBIT_MQ_HOST');
-        $this->port = getenv('RABBIT_MQ_PORT');
-        $this->user = getenv('RABBIT_MQ_USER');
-        $this->password = getenv('RABBIT_MQ_PASSWORD');
-        $this->connection = new AMQPStreamConnection($this->host, $this->port, $this->user, $this->password);
+        $config = config('rabbitmq');
+        $this->connection = new AMQPStreamConnection(
+            $config['host'],
+            $config['port'],
+            $config['user'],
+            $config['password'],
+            $config['vhost']
+        );
+        $this->channel = $this->connection->channel();
     }
 
     public function consume(string $queueName, Closure $callback): void
     {
         try {
-            echo "Initializing consumer of queue [$queueName]", " \n";
-            $channel = $this->createChannel($queueName);
-            $channel->basic_consume($queueName, '', false, false, false, false, $callback);
-            echo 'Waiting for messages...', " \n";
-            while (!empty($channel->callbacks)) {
-                $channel->wait();
+            echo "Initializing consumer of queue [$queueName]" . PHP_EOL;
+            $this->channel->queue_declare($queueName, false, true, false, false);
+            $this->channel->basic_consume($queueName, '', false, false, false, false, $callback);
+            echo 'Waiting for messages...' . PHP_EOL;
+            while (!empty($this->channel->callbacks)) {
+                $this->channel->wait();
             }
-            $channel->close();
+            $this->channel->close();
             $this->connection->close();
         } catch (Exception $e) {
-            echo "\nError: " . $e->getMessage() . "\n";
+            echo "\nError: {$e->getMessage()}" . PHP_EOL;
             $this->connection->close();
         }
     }
@@ -46,27 +48,15 @@ class RabbitMQService
     public function consumeAck(AMQPMessage $message, string $queueName): void
     {
         $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
-        echo "[x] Done $queueName. {$message->getBody()}";
-    }
+        echo "\n[x] Done {$queueName}. {$message->getBody()}" . PHP_EOL;
 
-    private function createChannel(string $queueName, bool $durable = false): AMQPChannel
-    {
-        $channel = $this->connection->channel();
-        $channel->queue_declare($queueName, false, $durable);
-        $channel->basic_qos(0, 1, false);
-        return $channel;
     }
 
     public function publish(string $queueName, $message): void
     {
-        $channel = $this->createChannel($queueName, true);
-        $channel->exchange_declare('default_exchange', 'direct', false, true, false);
-        $channel->queue_bind($queueName, 'default_exchange');
-        $msg = new AMQPMessage(json_encode($message));
-        $channel->basic_publish($msg);
-        $channel->close();
+        $this->channel->queue_declare($queueName, false, true, false, false);
+        $this->channel->basic_publish(new AMQPMessage(json_encode($message)), '', $queueName);
+        $this->channel->close();
         $this->connection->close();
     }
-
-
 }
